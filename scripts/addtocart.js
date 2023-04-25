9a587c8e0f81c3bdf64ee81db9c33a31510faf18
vaishnavi borkar <105918011+Vaishnavi-Borkar@users.noreply.github.com>
2023-04-25 22:13:53 +0530

    Update addtocart.js

--- scripts/addtocart.js
+++ scripts/addtocart.js
@@ -1,7 +1,6 @@
 import navbar from "../components/navbar.js";
 document.querySelector("#navbar").innerHTML = navbar();
 
-
 let data = JSON.parse(localStorage.getItem("cartData")) || [];
 function append(data) {
     let box = document.querySelector("#addtocart");
